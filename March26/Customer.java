f9365928262393d6739b600d2077971f9510113b
Thushara Shree <thusharashree2003@gmail.com>
2025-03-26 22:39:03 +0530

    static block and encryption

---
+++ March26/Customer.java
@@ -0,0 +1,23 @@
+package com.xworkz.internal;
+
+public class Customer {
+
+    private int cardNo;
+    private String name;
+
+    public int getCardNo(){
+        return this.cardNo;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setCardNo(int cardNo){
+        this.cardNo=cardNo;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+}
